91a3efe304b52bcfeebf2c8688b769f90e3c7f3a
abhay.deshpande <abhay.deshpande@softwaysolutions.com>
2022-01-23 23:26:26 +0530

    initializing modal

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Modal from "react-modal";
+import { useRouter } from "next/router";
+import TransferModal from "./modal/TransferModal"
 const Header = ({walletAddress,connectWallet}) => {
   return (
     <Wrapper>
---
+++ components/modal/TransferModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TransferModal = () => {
+  return <div></div>;
+};
+
+export default TransferModal;
