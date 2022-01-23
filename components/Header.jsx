cae4ea3501e8dfc3e38b09a27b6db8cdaad7367d
abhay.deshpande <abhay.deshpande@softwaysolutions.com>
2022-01-23 23:54:08 +0530

    modal design for transfer modal

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import Modal from "react-modal";
 import { useRouter } from "next/router";
-import TransferModal from "./modal/TransferModal"
-const Header = ({walletAddress,connectWallet}) => {
+import TransferModal from "./modal/TransferModal.jsx";
+import Link from "next/link";
+const Header = ({ walletAddress, connectWallet }) => {
+  const router = useRouter();
+  const customStyles={
+    content:{
+      top:"50%",
+      left:"50%",
+      right:"auto",
+      bottom:'auto',
+      transform:'translate(-50%,-50%)',
+      backgroundColor:"#0a0b0d",
+      padding:0,
+      border:'none'
+    },
+    overlay:{
+      backgroundColor:"rgba(10,11,13,0.75)"
+    }
+  }
   return (
     <Wrapper>
       <Title>Assets</Title>
@@ -12,15 +29,24 @@ const Header = ({walletAddress,connectWallet}) => {
           <WalletLinkTitle>
             Wallet Connected
             <WalletAddress>
-              {walletAddress.slice(0,7)}...{walletAddress.slice(35)}
+              {walletAddress.slice(0, 7)}...{walletAddress.slice(35)}
             </WalletAddress>
           </WalletLinkTitle>
         </WalletLink>
         <Button style={{ backgroundColor: "#3773f5", color: "#000" }}>
           Buy/Sell
         </Button>
+        <Link href={"/?transfer=1"}>
         <Button>Send / Receive</Button>
+        </Link>
       </ButtonContainer>
+      <Modal
+        isOpen={router.query.transfer ? true : false}
+        onRequestClose={() => router.push("/")}
+        style={customStyles}
+      >
+        <TransferModal/>
+      </Modal>
     </Wrapper>
   );
 };
@@ -57,26 +83,25 @@ const Button = styled.button`
   }
 `;
 
+const WalletLink = styled.div`
+  font-size: 0.8rem;
+  border: 1px solid #282b2f;
+  border-radius: 50rem;
+  font-size: 1.2rem;
+  margin-right: 1rem;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
 
-const WalletLink =styled.div`
-  font-size:0.8rem;
-  border:1px solid #282b2f;
-  border-radius : 50rem;
-  font-size:1.2rem;
-  margin-right:1rem;
-  padding:0 1rem;
-  display:flex;
-  flex-direction:column;
-  align-items:flex-start;
-  justify-content:center;
-`
-
-const WalletLinkTitle =styled.div`
-  font-size:1.1rem;
-  margin-bottom:0.3rem;
-  color:#27ad75;
-  font-weight:600;
-`
+const WalletLinkTitle = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.3rem;
+  color: #27ad75;
+  font-weight: 600;
+`;
 const WalletAddress = styled.div`
- font-size:0.8rem;
+  font-size: 0.8rem;
 `;
--- components/modal/TransferModal.jsx
+++ components/modal/TransferModal.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import styled from "styled-components";
 const TransferModal = () => {
-  return <div></div>;
+    const [action,setAction] =useState("send")
+
+    const selectedStyle={
+        color:"#3773f5"
+    }
+
+    const unselectedStyle={
+        border:"1px solid #282b2f"
+    }
+  return (
+    <Wrapper>
+      <Selector>
+        <Option style={action==="send" ? selectedStyle : unselectedStyle} onClick={()=>setAction("send")}>
+          <p>Send</p>
+        </Option>
+        <Option style={action==="receive" ? selectedStyle : unselectedStyle} onClick={()=>setAction("receive")}>
+          <p>Receive</p>
+        </Option>
+      </Selector>
+    </Wrapper>
+  );
 };
 
 export default TransferModal;
+
+const Wrapper = styled.div`
+  height: 35rem;
+  width: 27rem;
+  color: white;
+  border: 1px solid #282b2f;
+  display: flex;
+  flex-direction: column;
+`;
+const Selector = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 5rem;
+`;
+
+const Option = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  place-items: center;
+  font-size: 1.2rem;
+  font-weight: 600;
+  &:hover {
+    cursor: pointer;
+    background-color: #111214;
+  }
+`;
